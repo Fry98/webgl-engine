7d2431c00b26ada59820fddb39599bcc5aabacf4
Fry98 <filip-toman@seznam.cz>
2020-04-19 02:23:18 +0200

    Added Camera object

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,15 @@ import frag from './shaders/fragment.glsl';
 import KeyMap from './keymap';
 import { mat4, glMatrix } from 'gl-matrix';
 import { downloadMeshes, MeshMap } from '../node_modules/webgl-obj-loader/src/index';
+import Camera from './camera';
+import viewport from './viewport';
 
 const canv = document.getElementById('canv') as HTMLCanvasElement;
 const gl = canv.getContext('webgl2');
+const mouseMove = {
+  x: 0,
+  y: 0
+};
 let vertexShader = gl.createShader(gl.VERTEX_SHADER);
 let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 
@@ -14,11 +20,20 @@ gl.shaderSource(fragmentShader, frag);
 gl.compileShader(vertexShader);
 gl.compileShader(fragmentShader);
 
+const cam = new Camera([30, 5, 25], [0, 0, 1], 70);
+viewport(gl);
+canv.onclick = () => canv.requestPointerLock();
 const texture = new Image();
-texture.src = 'assets/dog_diffuse.jpg';
+texture.src = 'assets/terrain.png';
+
+document.onmousemove = e => {
+  if (document.pointerLockElement !== canv) return;
+  mouseMove.x += e.movementX;
+  mouseMove.y += e.movementY;
+};
 
 texture.onload = () => downloadMeshes({
-  dog: 'assets/dog.obj'
+  dog: 'assets/terrain.obj'
 }, main, {});
 
 function main(meshes: MeshMap) {
@@ -94,47 +109,45 @@ function main(meshes: MeshMap) {
   gl.useProgram(program);
 
   const mWorldLocation = gl.getUniformLocation(program, 'mWorld');
-  const mViewLocation = gl.getUniformLocation(program, 'mView');
-  const mProjLocation = gl.getUniformLocation(program, 'mProj');
+  const mViewProjectionLocation = gl.getUniformLocation(program, 'mViewProjection');
 
   const mWorld = new Float32Array(16);
-  const mView = new Float32Array(16);
-  const mProj = new Float32Array(16);
   mat4.identity(mWorld);
-  mat4.lookAt(mView, [0, 3, 5], [0, 1.5, 0], [0, 1, 0]);
-  mat4.perspective(mProj, glMatrix.toRadian(70), canv.width / canv.height, 0.1, 1000);
-  gl.uniformMatrix4fv(mProjLocation, false, mProj);
+  mat4.rotate(mWorld, mWorld, glMatrix.toRadian(-90), [1, 0, 0]);
+  mat4.translate(mWorld, mWorld, [-20, 0, 0]);
 
   const identity = new Float32Array(16);
   mat4.identity(identity);
   requestAnimationFrame(loop);
 
   function loop() {
-    if (KeyMap[37]) {
-      mat4.rotate(mWorld, mWorld, glMatrix.toRadian(2), [0, 1, 0]);
+    if (KeyMap[65]) {
+      cam.moveRight(-1);
     }
 
-    if (KeyMap[39]) {
-      mat4.rotate(mWorld, mWorld, glMatrix.toRadian(-2), [0, 1, 0]);
-    }
-
-    if (KeyMap[38]) {
-      mat4.translate(mView, mView, [0, 0, 0.1]);
-    }
-
-    if (KeyMap[40]) {
-      mat4.translate(mView, mView, [0, 0, -0.1]);
+    if (KeyMap[68]) {
+      cam.moveRight(1);
     }
 
     if (KeyMap[87]) {
-      mat4.rotate(mWorld, mWorld, glMatrix.toRadian(2), [1, 0, 0]);
+      cam.moveForward(1);
     }
 
     if (KeyMap[83]) {
-      mat4.rotate(mWorld, mWorld, glMatrix.toRadian(-2), [1, 0, 0]);
+      cam.moveForward(-1);
     }
 
-    gl.uniformMatrix4fv(mViewLocation, false, mView);
+    if (mouseMove.x !== 0) {
+      cam.rotateRight(mouseMove.x / 100);
+      mouseMove.x = 0;
+    }
+
+    if (mouseMove.y !== 0) {
+      cam.rotateUp(-mouseMove.y / 100);
+      mouseMove.y = 0;
+    }
+
+    gl.uniformMatrix4fv(mViewProjectionLocation, false, cam.getViewProjectionMatrix());
     gl.uniformMatrix4fv(mWorldLocation, false, mWorld);
     gl.clearColor(0.4, 0.4, 0.45, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -9,11 +9,10 @@ out vec2 fragTexCoord;
 out vec3 fragNormal;
 
 uniform mat4 mWorld;
-uniform mat4 mView;
-uniform mat4 mProj;
+uniform mat4 mViewProjection;
 
 void main() {
   fragNormal = (mWorld * vec4(vertNormal, 0.0)).xyz;
   fragTexCoord = vertTexCoord;
-  gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);
+  gl_Position = mViewProjection * mWorld * vec4(vertPosition, 1.0);
 }
